5f70ea17153e50fa150685b1f0e9249841bd1551
Lucas Bernardino <lucasblameu@gmail.com>
2021-12-30 08:06:22 -0300

    Adicionar arquivos de projeto.

---
+++ ExercHeritage1/Entities/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace ExercHeritage1.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHours { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHours)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHours = valuePerHours;
+        }
+
+        public virtual double Payment()
+        {
+            return ValuePerHours * Hours;
+        }
+    }
+}
---
+++ ExercHeritage1/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercHeritage1.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHours, double additionalCharge)
+            : base(name, hours, valuePerHours)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharge * 1.1;
+        }
+    }
+}
